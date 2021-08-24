26fa86c2a2251bef6697f94691d638ae63e368e9
SaeSimCheon <seobqod@gmail.com>
2021-08-24 21:40:07 +0900

    0824 week2 start

---
+++ Scripts/week2.sql
@@ -0,0 +1,17 @@
+/* Basic week2 01 */
+
+select * from basket_a  ; 
+select * from basket_b  ; 
+
+
+/* Basic week2 02 */
+/* Basic week2 03 */
+/* Basic week2 04 */
+/* Basic week2 05 */
+/* Basic week2 06 */
+/* Basic week2 07 */
+/* Basic week2 08 */
+/* Basic week2 09 */
+/* Basic week2 10 */
+/* Basic week2 11 */
+/* Basic week2 12 */
